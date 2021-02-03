1b9ff9b17c2fb1e701716d4fdd2666d067043bed
Yankd <1126223345@qq.com>
2021-02-03 18:47:09 +0800

    集成mybatis分页插件，页码从1开始

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -4,7 +4,7 @@ import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
-import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +22,7 @@ public class ChapterService {
 
     private ChapterMapper chapterMapper;
     public List<ChapterDto> list(){
+        PageHelper.startPage (1,1);
         ChapterExample example = new ChapterExample ();
         List<Chapter> chapterList = chapterMapper.selectByExample (example);
         List<ChapterDto> chapterDtolist = new ArrayList<ChapterDto>();
