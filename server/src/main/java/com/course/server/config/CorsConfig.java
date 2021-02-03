f64e88b8c44e10f1ffcb55dcd30028b7900f7979
Yankd <1126223345@qq.com>
2021-02-03 15:35:44 +0800

    使用负载均衡lb://+注册中心的服务名称来优化体验（loadbalance），修改springboot版本从2.4.2到2.2.2.RELEASE 修改springcloud版本2020.0.0到Hoxton.RELEASE，困扰了一整天的corsfilter bug已解决。

--- server/src/main/java/com/course/server/config/CorsConfig.java
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -1,24 +1,24 @@
-package com.course.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- *
- * @auther Yankd
- * @date 2021/2/1 0001 12:27
- */
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping ("/**")
-                .allowedOrigins ("/")
-                .allowedHeaders (CorsConfiguration.ALL)
-                .allowedMethods (CorsConfiguration.ALL)
-                .allowCredentials (true)
-                .maxAge (3600);//一小时内不需要再预检（发option请求）
-    }
-}
+//package com.course.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// *
+// * @auther Yankd
+// * @date 2021/2/1 0001 12:27
+// */
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping ("/**")
+//                .allowedOrigins ("/")
+//                .allowedHeaders (CorsConfiguration.ALL)
+//                .allowedMethods (CorsConfiguration.ALL)
+//                .allowCredentials (true)
+//                .maxAge (3600);//一小时内不需要再预检（发option请求）
+//    }
+//}
