df1df5553d81eda5190dee6b4c745b6a71f67f1e
Yankd <1126223345@qq.com>
2021-02-13 13:50:36 +0800

    增加修改大章功能，新增和修改用的是同一个保存功能，通过传入的参数有没有id来判断。

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -18,9 +18,9 @@
       <tr>
         <th>ID</th>
         <th>名称</th>
-        <th>课程</th>
+        <th>课程ID</th>
         <th>操作</th>
-      </tr>ID
+      </tr>
       </thead>
 
       <tbody>
@@ -31,20 +31,12 @@
 
         <td>
           <div class="hidden-sm hidden-xs btn-group">
-            <button class="btn btn-xs btn-success">
-              <i class="ace-icon fa fa-check bigger-120"></i>
-            </button>
-
-            <button class="btn btn-xs btn-info">
+            <button v-on:click="edit(chapter)" class="btn btn-xs btn-info">
               <i class="ace-icon fa fa-pencil bigger-120"></i>
             </button>
 
             <button class="btn btn-xs btn-danger">
               <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
-
-            <button class="btn btn-xs btn-warning">
-              <i class="ace-icon fa fa-flag bigger-120"></i>
             </button>
           </div>
 
@@ -246,6 +238,13 @@
     methods: {
       add (){
         let _this = this;
+        $("#form-modal").modal("show");
+        _this.chapter = {};
+      },
+      edit (chapter){
+        let _this = this;
+       _this.chapter = chapter;
+       _this.chapter = $.extend({},chapter);
         $("#form-modal").modal("show");
       },
 
