090940a4941b5985a44e621c15dd9bca9491c0f8
Yankd <1126223345@qq.com>
2021-02-13 12:16:13 +0800

    后端纯接口的统一返回实体类ReponseDto， chapter保存成功后刷新列表，为modal增加id属性

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -2,6 +2,7 @@ package com.course.business.controller.admin;
 
 import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
+import com.course.server.dto.ResponseDto;
 import com.course.server.service.ChapterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,15 +25,20 @@ public class ChapterController {
     private ChapterService chapterService;
 
     @RequestMapping("/list")
-    public PageDto list( @RequestBody PageDto pageDto) {
+    public ResponseDto list( @RequestBody PageDto pageDto) {
         LOG.info ("pageDto的值: {}",pageDto );
+        //下面方法做了pageDto.set(),最终只是返回pageDto实例;
         chapterService.list (pageDto);
-        return pageDto;
+        ResponseDto responseDto = new ResponseDto ();
+        responseDto.setContent (pageDto);
+        return responseDto;
     }
     @RequestMapping("/save")
-    public ChapterDto save(@RequestBody ChapterDto chapterDto) {
+    public ResponseDto save(@RequestBody ChapterDto chapterDto) {
         LOG.info ("chapterDto的值: {}",chapterDto );
         chapterService.save (chapterDto);
-        return chapterDto;
+        ResponseDto responseDto = new ResponseDto ();
+        responseDto.setContent (chapterDto);
+        return responseDto;
     }
 }
---
+++ server/src/main/java/com/course/server/dto/ResponseDto.java
@@ -0,0 +1,59 @@
+package com.course.server.dto;
+
+/**
+ * @auther Yankd
+ * @date 2021/2/13 0013 11:34
+ */
+public class ResponseDto<T> {
+    /**
+     *业务上的成功或者失败
+     */
+    private boolean success = true;
+    /**
+     * 返回码
+     */
+    private String code;
+    /**
+     * 返回信息
+     */
+    private String message;
+    /**
+     * 返回泛型数据，自定义类型
+     */
+    private T content;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseDto{" +
+                "success=" + success +
+                ", code='" + code + '\'' +
+                ", message='" + message + '\'' +
+                ", content=" + content +
+                '}';
+    }
+}
